71a3b538b7cfaa48bb576ebe670c60cafe1a5a6d
C Vera <cvera08@gmail.com>
2021-06-28 19:52:59 -0300

    Changed Today's Date to format "dd month, year" (es-ES)

--- date.js
+++ date.js
@@ -1,11 +1,10 @@
 function printDateInTodaysDateID() {
     var today = new Date();
     var dd = String(today.getDate()).padStart(2, '0');
-    var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+    var mm = today.toLocaleString('es-ES', { month: 'long' }); //Source: https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Global_Objects/Date/toLocaleDateString
     var yyyy = today.getFullYear();
 
-    today = mm + '/' + dd + '/' + yyyy;
+    today = dd + ' ' + mm + ', ' + yyyy;
 
     document.getElementById("todaysDate").innerHTML = today;
 } //Source: https://stackoverflow.com/questions/16167675/return-the-value-of-a-js-function-and-use-it-as-the-value-for-an-input-button
-
