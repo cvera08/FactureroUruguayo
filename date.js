d76dc995cd143e94b940289dc0ca1fff056be8ef
CV <carlos.vera@distillery.com>
2021-07-05 15:06:39 -0300

    Todays date with double display Fix in start loading of due date Spanish version Recipient data also

--- date.js
+++ date.js
@@ -1,12 +1,11 @@
 function printDateInTodaysDateID() {
-    document.getElementById("todaysDate").innerHTML = getDateInSpFormat(new Date());
-} 
+    document.getElementById("todaysDate").value = getDateInYYYMMDDFormat(new Date());
+    document.getElementById("spTodaysDate").innerHTML = getDateInSpFormat(new Date());
+}
 
 function printDatePlus15InDueDate() {
-    var today15 = addDays(new Date(), 15); //Adding 15 days to today
-    todayYYYmmDD= today15.toISOString().split('T')[0]; //converting to yyyy-MM-dd format
-
-    document.getElementById("dpDueDate").value = todayYYYmmDD;
+    var today15 = addDays(new Date(), 15); //Adding 15 days to today on purpose
+    document.getElementById("dpDueDate").value = getDateInYYYMMDDFormat(today15);
     document.getElementById("spDueDate").innerHTML = getDateInSpFormat(today15);
 }
 
@@ -16,7 +15,13 @@ function changeDP() {
     document.getElementById("spDueDate").innerHTML = getDateInSpFormat(selectedDateFixed);
 }
 
-function getDateInSpFormat(originalDate){
+function changeTodaysDateInputSpanishFormat() {
+    var selectedDate = document.getElementById("todaysDate").value;
+    var selectedDateFixed = addDays(new Date(selectedDate), 1); //Because if not we loose 1 day (UTC vs GMT vs GMT-3)
+    document.getElementById("spTodaysDate").innerHTML = getDateInSpFormat(selectedDateFixed);
+}
+
+function getDateInSpFormat(originalDate) {
     date = new Date(originalDate);
     var dd = String(date.getDate()).padStart(2, '0');
     var mm = date.toLocaleString('es-ES', { month: 'long' }); //Source: https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Global_Objects/Date/toLocaleDateString
@@ -27,8 +32,18 @@ function getDateInSpFormat(originalDate){
 } //Source: https://stackoverflow.com/questions/16167675/return-the-value-of-a-js-function-and-use-it-as-the-value-for-an-input-button
 
 
+function getDateInYYYMMDDFormat(originalDate) { // I won't use new Date().toISOString().split('T')[0]; because generates issues with internationalization + converting dates (GMT vs GMT-3 vs UTC vs ISO)
+    date = new Date(originalDate);
+    var dd = String(date.getDate()).padStart(2, '0');
+    var mm = String(date.getMonth()+1).padStart(2, '0');
+    var yyyy = date.getFullYear();
+
+    formattedDate = yyyy + '-' + mm + '-' + dd;
+    return formattedDate;
+}
+
 function addDays(date, days) {
     var result = new Date(date);
     result.setDate(result.getDate() + days);
     return result;
-  }
+}
